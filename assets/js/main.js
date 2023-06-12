8e0c83aea5760e3ce2f3dcd89ec446ae5f9e105e
Masha <masha@Mashas-Air.lan>
2023-06-13 00:09:07 +0100

    programme

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,3 +135,21 @@
 		});
 
 })(jQuery);
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+			this.textContent = '[show abstract]';
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+			this.textContent = '[hide abstract]';
+    }
+  });
+}
